4cdb1206d19fc063374c573bf519f9d27008d8bb
Sam Smoot <ssmoot@gmail.com>
2009-01-12 00:25:35 -0600

    Added (and tested) internal Uri class to prepare for registering repositories.

--- lib/wheels/orm.rb
+++ lib/wheels/orm.rb
@@ -6,9 +6,19 @@ require Pathname(__FILE__).dirname.parent + "wieck" + "assertions"
 require Pathname(__FILE__).dirname.parent + "wieck" + "blank"
 require Pathname(__FILE__).dirname.parent + "wieck" + "ordered_set"
 
+require Pathname(__FILE__).dirname + "orm" + "uri"
+
 require Pathname(__FILE__).dirname + "orm" + "type"
 require Pathname(__FILE__).dirname + "orm" + "types"
 require Pathname(__FILE__).dirname + "orm" + "mappings" + "source"
 require Pathname(__FILE__).dirname + "orm" + "mappings" + "relation"
 
 require Pathname(__FILE__).dirname + "orm" + "repositories" + "abstract"
+
+module Wheels
+  module Orm
+    def self.connect(uri)
+      
+    end
+  end
+end
