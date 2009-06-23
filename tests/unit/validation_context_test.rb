daf5019cbc8bed65b5d211080a2ef75dba91fd8d
Sam Smoot <ssmoot@gmail.com>
2009-06-23 17:25:01 -0500

    Whitespace cleanup.

--- tests/unit/validation_context_test.rb
+++ tests/unit/validation_context_test.rb
@@ -3,13 +3,7 @@ require Pathname(__FILE__).dirname.parent + "helper"
 require 'ostruct'
 
 class ValidationContextTest < Test::Unit::TestCase
-  
-  def setup
-  end
 
-  def teardown
-  end
-  
   def test_validation_block_executed
     @executed = false
     validation_block = lambda { |check| @executed = true }
