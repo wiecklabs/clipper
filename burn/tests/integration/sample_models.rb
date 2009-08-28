d443aae32ea41afca1f459dc5e5f2c3394e91803
Sam Smoot <ssmoot@gmail.com>
2009-08-28 15:44:22 -0500

    Just an update to the Person model sample for current class declaration and mapping syntax.

--- burn/tests/integration/sample_models.rb
+++ burn/tests/integration/sample_models.rb
@@ -5,13 +5,20 @@ module Integration::SampleModels
   class Zoo; end
   class ZooKeeper; end
   class Exhibit; end
-
+  
   class Person
+    include Clipper::Accessors
+    
+    accessor :id => Integer
+    accessor :name => String
+    
     include Clipper::Model
 
     orm.map(self, "people") do |people|
-      people.key(people.field("id", Clipper::Types::Serial))
-      people.field("enabled", Clipper::Types::Integer, 1)
+      people.key(
+        people.field("id", Clipper::Repositories::Jdbc::Postgres::Serial.new)
+      )
+      people.field("name", Clipper::Repositories::Jdbc::Postgres::String.new(200))
     end
   end
 
